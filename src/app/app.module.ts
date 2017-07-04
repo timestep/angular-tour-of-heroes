a7d6ce37195a9a57957d5a9a4bc811f43efe300e
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2017-07-04 14:47:42 -0400

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './hero.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +16,13 @@ import { HeroService } from './hero.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent,
+      }
+    ])
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
