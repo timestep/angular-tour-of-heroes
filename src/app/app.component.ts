d2bd39710e15b41921ec474e1636af276c38f67e
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2017-07-04 13:25:01 -0400

    The Hero Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
