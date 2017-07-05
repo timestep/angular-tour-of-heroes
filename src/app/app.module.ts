d26c4e1064d6dbb27ab2644448c7ca4c895b5219
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2017-07-05 10:50:46 -0400

    last

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,31 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
         path: 'heroes',
         component: HeroesComponent,
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
       }
     ])
   ],
